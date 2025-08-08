7ab9b189e72018e20b7c56005e73ce1c4ae82005
cannonfodderalt <cannonfodderalt@gmail.com>
2025-08-07 21:21:12 -0700

    make api url more robust

--- components/MapWrapper.tsx
+++ components/MapWrapper.tsx
@@ -2,16 +2,17 @@ import React, { useEffect, useState } from "react";
 import { ActivityIndicator, View } from "react-native";
 import MapScreen from "./MapScreen";
 
-const API_URL = "http://192.168.68.161:8000/spots/";
+const API_URL = "http://192.168.68.161:8000";
 
 export default function MapWrapper() {
   const [spots, setSpots] = useState([]);
+  const [criteria, setCriteria] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchSpots = async () => {
       try {
-        const response = await fetch(API_URL);
+        const response = await fetch(API_URL + '/spots/');
         const data = await response.json();
         setSpots(data);
       } catch (err) {
@@ -24,6 +25,20 @@ export default function MapWrapper() {
     fetchSpots();
   }, []);
 
+  useEffect(() => {
+    const fetchCriteria = async () => {
+      try {
+        const response = await fetch(API_URL + '/criteria/');
+        const data = await response.json();
+        setCriteria(data);
+      } catch (err) {
+        console.error("Failed to fetch criteria:", err);
+      }
+    };
+
+    fetchCriteria();
+  }, []);
+
   if (loading) {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -32,5 +47,5 @@ export default function MapWrapper() {
     );
   }
 
-  return <MapScreen spots={spots} />;
+  return <MapScreen spots={spots} criteria={criteria} />;
 }
