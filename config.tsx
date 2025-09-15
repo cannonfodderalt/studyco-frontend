8e929a728982abe099df45afddbed092925540d7
cannonfodderalt <cannonfodderalt@gmail.com>
2025-09-14 17:22:53 -0700

    remove ip

--- config.tsx
+++ config.tsx
@@ -1,3 +1,1 @@
-//export const API_URL = "http://172.20.10.2:8000";
-//export const API_URL = "http://192.168.68.161:8000";
 export const API_URL = "http://studyco-ghf9anhyf7g0fcgb.westus3-01.azurewebsites.net";
